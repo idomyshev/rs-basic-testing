13415a9a33ecadcd3ef75ff9a1560dc779999edf
idomyshev <iadomyshev@gmail.com>
2024-09-13 17:03:28 +0200

    feat: table-tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,31 +1,32 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
-
-import { simpleCalculator } from "./index";
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    const result = simpleCalculator({ a: 2, b: 3, action: '+' });
+    const result = simpleCalculator({ a: 2, b: 3, action: Action.Add });
     expect(result).toBe(5);
   });
 
   test('should subtract two numbers', () => {
-    const result = simpleCalculator({ a: 10, b: 3, action: '-' });
+    const result = simpleCalculator({ a: 10, b: 3, action: Action.Subtract });
     expect(result).toBe(7);
   });
 
   test('should multiply two numbers', () => {
-    const result = simpleCalculator({ a: 8, b: 3, action: '*' });
+    const result = simpleCalculator({ a: 8, b: 3, action: Action.Multiply });
     expect(result).toBe(24);
   });
 
   test('should divide two numbers', () => {
-    const result = simpleCalculator({ a: 44, b: 4, action: '/' });
+    const result = simpleCalculator({ a: 44, b: 4, action: Action.Divide });
     expect(result).toBe(11);
   });
 
   test('should exponentiate two numbers', () => {
-    const result = simpleCalculator({ a: 2, b: 3, action: '^' });
+    const result = simpleCalculator({
+      a: 2,
+      b: 3,
+      action: Action.Exponentiate,
+    });
     expect(result).toBe(8);
   });
 
