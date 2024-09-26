a8765963e1a474e27c828a943bc5c10e2a168553
idomyshev <iadomyshev@gmail.com>
2024-09-26 15:23:20 +0200

    feat: lib api tests - ready

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,39 +1,31 @@
 import { throttledGetDataFromApi } from './index';
 import axios from 'axios';
-// import axios from 'axios';
 
-// const generateRandomString = (length = 10) => {
-//   let resultString = '';
-//   const characters = 'abcdefghijklmnopqrstuvwxyz';
-//   for (let i = 1; i <= length; i++) {
-//     resultString += characters.charAt(
-//       Math.floor(Math.random() * characters.length),
-//     );
-//   }
-//   return resultString;
-// };
+const generateRandomString = (length = 10) => {
+  let resultString = '';
+  const characters = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i = 1; i <= length; i++) {
+    resultString += characters.charAt(
+      Math.floor(Math.random() * characters.length),
+    );
+  }
+  return resultString;
+};
 
 jest.mock('axios');
 
-// jest.mock('axios', () => {
-//   const originalModule = jest.requireActual<typeof axios>('axios');
-//   return {
-//     ...originalModule,
-//     // create: (params: { baseURL: string }): string => {
-//     //   const { baseURL } = params;
-//     //   console.log('baseURLLLLL', baseURL);
-//     //   return baseURL;
-//     // },
-//     get: (): Promise<string> => {
-//       return new Promise((resolve) => {
-//         console.log('RUUUN GET');
-//         resolve('ABC');
-//       });
-//     },
-//   };
-// });
+jest.mock('lodash', () => {
+  const originalModule = jest.requireActual<typeof axios>('lodash');
+  return {
+    ...originalModule,
+    throttle: jest.fn((fn) => fn),
+  };
+});
 
 describe('throttledGetDataFromApi', () => {
+  let expectedRelativePath: string;
+  let expectedResponse: string;
+
   beforeAll(() => {
     jest.useFakeTimers();
   });
@@ -42,58 +34,53 @@ describe('throttledGetDataFromApi', () => {
     jest.useRealTimers();
   });
 
-  const expectedRelativePath = 'relativeeee1';
-  const expectedResponse = 'respoooonse1';
+  beforeEach(() => {
+    expectedRelativePath = generateRandomString();
+    expectedResponse = generateRandomString();
+  });
 
   test('should create instance with provided base url', async () => {
-    //axios.create = jest.fn();
-    // const methodGet = jest.fn().mockResolvedValue({ data: expectedResponse });
-    //
-    // (axios.create as jest.Mock).mockReturnValue({
-    //   get: methodGet,
-    // });
-    //
-    // const spyFunc = jest.spyOn(axios, 'create');
-    //
-    // await throttledGetDataFromApi(expectedRelativePath);
-    //
-    // expect(spyFunc).toHaveBeenCalledWith({
-    //   baseURL: 'https://jsonplaceholder.typicode.com',
-    // });
-    //console.log(generateRandomString());
-    // const expectedRelativePath = generateRandomString();
-    // const expectedResponse = generateRandomString();
-    //
-    // const methodGet = jest.fn().mockResolvedValue({ data: expectedResponse });
-    //
-    // (axios.create as jest.Mock).mockReturnValue({
-    //   get: methodGet,
-    // });
-    //
-    // expect(await throttledGetDataFromApi(expectedRelativePath)).toBe(
-    //   expectedResponse,
-    // );
-    //
-    // expect(methodGet).toHaveBeenCalledWith(expectedRelativePath);
+    const mockAxiosGet = jest
+      .fn()
+      .mockResolvedValue({ data: expectedResponse });
+
+    (axios.create as jest.Mock).mockReturnValue({
+      get: mockAxiosGet,
+    });
+
+    await throttledGetDataFromApi(expectedRelativePath);
+    const spyFunc = jest.spyOn(axios, 'create');
+
+    await throttledGetDataFromApi(expectedRelativePath);
+
+    expect(spyFunc).toHaveBeenCalledWith(
+      expect.objectContaining({
+        baseURL: expect.stringMatching(/^(http|https):\/\//),
+      }),
+    );
   });
 
   test('should perform request to correct provided url', async () => {
-    const methodGet = jest.fn().mockResolvedValue({ data: expectedResponse });
+    const mockAxiosGet = jest
+      .fn()
+      .mockResolvedValue({ data: expectedResponse });
 
     (axios.create as jest.Mock).mockReturnValue({
-      get: methodGet,
+      get: mockAxiosGet,
     });
 
     await throttledGetDataFromApi(expectedRelativePath);
 
-    expect(methodGet).toHaveBeenCalledWith(expectedRelativePath);
+    expect(mockAxiosGet).toHaveBeenCalledWith(expectedRelativePath);
   });
 
   test('should return response data', async () => {
-    const methodGet = jest.fn().mockResolvedValue({ data: expectedResponse });
+    const mockAxiosGet = jest
+      .fn()
+      .mockResolvedValue({ data: expectedResponse });
 
     (axios.create as jest.Mock).mockReturnValue({
-      get: methodGet,
+      get: mockAxiosGet,
     });
 
     const response = await throttledGetDataFromApi(expectedRelativePath);
